fcc28877bf5be9fd769702054edd499a06c8a598
Mortens4444 <mortens.4444@gmail.com>
2019-04-22 00:25:27 +0200

    Initial commit

---
+++ KillProcesses/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System;
+using System.Windows.Forms;
+
+namespace KillProcesses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                var processes = Process.GetProcessesByName(args[0]);
+                foreach (var process in processes)
+                {
+                    process.Kill();
+                    process.Close();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Process name should be provided. Eg.: {0} notepad", Application.ProductName);
+            }
+        }
+    }
+}
